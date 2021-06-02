4e65ac707545c52d1f3b220311b0a6193231d965
anakuron <antti.nissinen@gmail.com>
2021-06-02 12:22:10 +0300

    added the upload script

---
+++ Public/upload.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+   <html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <title>502</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes"/>
+      <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+   </head>
+   <style>
+      #black-bg {background: linear-gradient(to bottom right, #222222,#2b2b2b) fixed;
+         min-height:100vh;}
+   </style>
+<body>
+<div id="black-bg">
+        <br>
+        <div class="ui inverted center aligned container segment">
+            <div class="ui content">
+<?php
+
+// folder thingy
+$folder = $_POST['folder'];
+$dirPath = 'gallery/'.$folder;
+$result = mkdir($dirPath);
+
+if ($result == '1') {
+
+echo $dirPath . " has been created<br>";
+
+} else {
+echo $dirPath . " has not been created<br>";
+}
+
+
+//$target_dir = "uploads/";
+$target_dir = $dirPath."/";
+$target_file = $target_dir. basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".<br>";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.<br>";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "file already exists.<br>";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000*16) {
+    echo "your file is too large.<br>";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" && $imageFileType != "bmp" ) {
+    echo "only JPG, JPEG, PNG, BMP & GIF files are allowed.<br>";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "<h1>your file was not uploaded.</h1>";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+
+
+        $output = shell_exec("./mogrify.sh $target_file $folder");
+        
+
+        $previousPage = $_SERVER["HTTP_REFERER"];
+        header('Location: '.$previousPage);
+
+    } else {
+        echo "there was an error uploading your file.<br>";
+    }
+}
+?>
+</div>
+</div>
+</div>
+
+
+</body>
