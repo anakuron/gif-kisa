772000d9664e502a9c2e959aef3920284fe6ca56
anakuron <antti.nissinen@gmail.com>
2021-06-17 16:19:40 +0300

    added mogrify script for converting image previews

---
+++ public/mogrify.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#this will convert the uploaded images to smaller versions using mogrify
+filepath=$1
+user=$2
+
+echo "phplta saatu kuvapath on: $1" >> php_upload_log.txt
+echo "phplta saatu user on: $2" >> php_upload_log.txt
+
+string=$filepath
+prefix="gallery/"
+path1=${string#"$prefix"}
+path2=gallery-preview/$path1
+
+cp --backup=existing $filepath gallery-original/;
+mkdir -p gallery-preview/$user;
+cp -r $filepath gallery-preview/$user;
+mogrify -resize 512x512 -auto-orient -quality 90 $path2;
+mogrify -resize 1080x1080 -auto-orient -quality 70 $filepath;
+
+
+echo "mogrifyn preview path on: $path2" >> php_upload_log.txt
+echo "mogrifyn gallery path on: $filepath" >> php_upload_log.txt
+echo "end of log" >> php_upload_log.txt
